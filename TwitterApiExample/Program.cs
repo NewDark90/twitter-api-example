822baf355e3e9b9367dcd08f823e8d70302bf8fb
Zachary Dow <mr.zachary.dow@gmail.com>
2022-10-01 18:28:01 -0700

    Moving the project down a folder

--- TwitterApiExample/Program.cs
+++ TwitterApiExample/Program.cs
@@ -5,16 +5,14 @@ using Tweetinvi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<ITweetRepository, TweetRepository>();
 builder.Services.AddScoped<IReadOnlyConsumerCredentials, TwitterConsumerCredentials>();
 builder.Services.AddScoped<ITwitterClient, TwitterClient>();
 builder.Services.AddHostedService<TwitterStreamService>();
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
